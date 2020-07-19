ae5041d6e61ae9ee754cf715eb99ea3141fb081c
hith-hj <hjhaithem@gmail>
2020-07-19 14:41:35 +0300

    updated

--- resources/views/livewire/topnav.blade.php
+++ resources/views/livewire/topnav.blade.php
@@ -145,10 +145,10 @@
                     <a class="dropdown-item" wire:click="$emitTo('main', 'changeBody', ['profile','{{Auth::id()}}'])">
                         <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i> Profile
                     </a>
-                    <a class="dropdown-item" href="#">
+                    {{--<a class="dropdown-item" href="#">
                         <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i> Settings
                     </a>
-                    {{-- <a class="dropdown-item" href="#">
+                     <a class="dropdown-item" href="#">
                         <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i> Activity Log 
                     </a> --}}
                     <div class="dropdown-divider"></div>
